420385466cfd1f5964207b57f72c209348312d62
bhush-r <bhushanlilhare956@gmail.com>
2024-05-30 16:53:02 +0530

    add particeles

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,29 +1,27 @@
+// src/components/home/Home.jsx
 import React from 'react';
 import "./home.css";
 import Me from "../../assets/avatar-1.svg";
 import HeaderSocials from "./HeaderSocials";
 import ScrollDown from "./ScrollDown";
-import Shapes from "./Shapes";
-
+import ParticlesComponent from "./ParticlesComponent"; // Correct import path
 
 const Home = () => {
-    return (
-        <section className="home container" id="home">
-        <div className="intro">
-          <img src={Me} alt="" className="home__img" />
-          <h1 className="home__name">Bhushan Lilhare</h1>
-           <span className="home__education">I'm a Java developer</span>
+  return (
+    <section className="home container" id="home">
+      <ParticlesComponent id="tsparticles" /> {/* Place ParticlesComponent outside the intro div */}
+      <div className="intro">
+        <img src={Me} alt="" className="home__img" />
+        <h1 className="home__name">Bhushan Lilhare</h1>
+        <span className="home__education">I'm a Java developer</span>
 
-        
-          <HeaderSocials />
+        <HeaderSocials />
 
-          <a href="#contact" className="btn">Hire Me</a>
-        <ScrollDown/>
-        </div>
-    
-        <Shapes />
-      </section>
-    )
+        <a href="#contact" className="btn">Hire Me</a>
+        <ScrollDown />
+      </div>
+    </section>
+  );
 }
 
-export default Home
+export default Home;
